db3e14bf0c11663e30422cfc53fc79ffd37b4790
Adib Bin Khaled <adibkhaled@gmail.com>
2026-03-13 12:24:48 +0100

    Add and update default page timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,6 +21,7 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
+  timeout: 120 * 1000,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html', { outputFolder: 'playwright-report' }],
